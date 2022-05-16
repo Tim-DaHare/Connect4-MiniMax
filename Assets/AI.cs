7103a3ca7df63f9aa5557960d9e4053b92abddd9
Tim <tim.de.haas@hotmail.nl>
2022-05-16 15:28:20 +0200

    added method required by minimax algorithm

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -3,8 +3,31 @@ using UnityEngine;
 
 public class Node
 {
-    public CellState[,] _grid;
-    public List<Node> _children;
+    public CellState[,] grid;
+    public List<Node> children;
+    public Node parent;
+
+    public int evalValue;
+
+    public int Evaluate()
+    {
+        return 1;
+    }
+
+    public Node(CellState[,] cGrid, Node cParent = null)
+    {
+        grid = cGrid.Clone() as CellState[,];
+        parent = cParent;
+    }
+
+    public void InitializeChildren(int childCount, int gridWidth, int gridHeight)
+    {
+        for (int i = 0; i < childCount; i++)
+        {
+            var g = new CellState[gridWidth, gridHeight];
+            children.Add(new Node(g, this));
+        }
+    }
 }
 
 public static class AI
@@ -20,34 +43,32 @@ public static class AI
     /// <returns></returns>
     public static int MiniMax(Node node, int depth, int alpha, int beta, bool isMax)
     {
-        if (depth == 0 || true) // add method which determines if the game is over 
-            return 1; // replace 1 with value of usefulness
+        if (depth == 0 || ConnectFour.IsGameOver(node.grid))
+            return node.Evaluate();
 
         if (isMax)
         {
             var maxEval = int.MinValue;
-            foreach (var child in node._children)
+            foreach (var child in node.children)
             {
                 var eval = MiniMax(child, depth -1, alpha, beta, false);
                 maxEval = Mathf.Max(maxEval, eval);
                 alpha = Mathf.Max(alpha, eval);
                 if (beta <= alpha)
                     break;
-                return maxEval;
             }
+            return maxEval;
         }
-        else
+
+        var minEval = int.MaxValue;
+        foreach (var child in node.children)
         {
-            var minEval = int.MaxValue;
-            foreach (var child in node._children)
-            {
-                var eval = MiniMax(child, depth -1, alpha, beta, true);
-                minEval = Mathf.Min(minEval, eval);
-                beta = Mathf.Min(minEval, eval);
-                if (beta <= alpha)
-                    break;
-                return minEval;
-            }
+            var eval = MiniMax(child, depth -1, alpha, beta, true);
+            minEval = Mathf.Min(minEval, eval);
+            beta = Mathf.Min(minEval, eval);
+            if (beta <= alpha)
+                break;
         }
+        return minEval;
     }
 }
--- Assets/ConnectFour.cs
+++ Assets/ConnectFour.cs
@@ -11,12 +11,15 @@ public class ConnectFour : MonoBehaviour
 {
     private Texture2D _boardTex;
     private Material _material;
-
+    
     private int _gridWidth = 7;
     private int _gridHeight = 6;
-
+    
     private CellState[,] _grid;
-
+    
+    // private bool _playerStarts;
+    private bool _isPlayersTurn = true;
+    
     private void Start()
     {
         _grid = new CellState[_gridWidth, _gridHeight];
@@ -26,9 +29,22 @@ public class ConnectFour : MonoBehaviour
             filterMode = FilterMode.Point
         };
         _material.mainTexture = _boardTex;
+        // PlaceChip(0, CellState.Red);
+        // PlaceChip(0, CellState.Red);
+        // PlaceChip(0, CellState.Red);
+        // PlaceChip(1, CellState.Red);
+        // PlaceChip(1, CellState.Red);
+        // PlaceChip(2, CellState.Red);
+        //
+        // PlaceChip(4, CellState.Red);
+        // PlaceChip(5, CellState.Red);
+        // PlaceChip(5, CellState.Red);
+        // PlaceChip(6, CellState.Red);
+        // PlaceChip(6, CellState.Red);
+        // PlaceChip(6, CellState.Red);
         UpdateGridTexture();
     }
-    
+
     private void UpdateGridTexture()
     {
         for (var x = 0; x < _gridWidth; x++)
@@ -46,12 +62,44 @@ public class ConnectFour : MonoBehaviour
         _boardTex.Apply();
     }
 
-    public void PlaceChipBlue(int columnIndex)
+    private void AITurn()
     {
-        PlaceChip(columnIndex, CellState.Blue);
+        var node = new Node(_grid);
+        
+        var sTime = Time.time;
+
+        node.InitializeChildren(_gridWidth, _gridWidth, _gridHeight);
+
+        // calculate best move and get col index for that move
+        var tarEval = AI.MiniMax(node, 3, int.MinValue, int.MaxValue, true);
+        var tarCol = node.children.FindIndex(n => n.evalValue == tarEval);
+        
+        PlaceChip(tarCol, CellState.Red);
+        
+        print($"AI took {Time.time - sTime} seconds to decide their turn");
+        
+        _isPlayersTurn = true;
     }
 
-    private void PlaceChip(int columnIndex, CellState color)
+    public void PlaceChipBlue(int columnIndex)
+    {
+        // if (!_isPlayersTurn) return;
+        
+        PlaceChip(columnIndex, CellState.Blue);
+        var o = IsGameOver(_grid);
+        print(o);
+        // _isPlayersTurn = false;
+
+        // AITurn();
+    }
+
+    /// <summary>
+    /// Method to place a chip of a specified color into a column of the grid
+    /// </summary>
+    /// <param name="columnIndex"></param>
+    /// <param name="color"></param>
+    /// <returns>A boolean representing if placement is a success</returns>
+    private bool PlaceChip(int columnIndex, CellState color)
     {
         for (var y = _gridHeight - 1; y >= 0; y--)
         {
@@ -59,15 +107,99 @@ public class ConnectFour : MonoBehaviour
             {
                 _grid[columnIndex, y] = color;
                 UpdateGridTexture();
-                return;
+                return true;
             }
             
             if (_grid[columnIndex, y - 1] != CellState.Empty)
             {
                 _grid[columnIndex, y] = color;
                 UpdateGridTexture();
-                return;
+                return true;
             }
         }
+
+        return false;
+    }
+
+    public static bool IsGameOver(CellState[,] grid)
+    {
+        var gridWidth = grid.GetLength(0);
+        var gridHeight = grid.GetLength(1);
+        
+        // Horizontal scan for 4 chips across the grid
+        for (var x = 0; x < gridWidth; x++)
+            for (var y = 0; y < gridHeight; y++)
+            {
+                var startColor = grid[x, y];
+                if (startColor == CellState.Empty) continue; // Skip empty cells
+
+                var count = 0;
+                for (var s = 0; s < 4; s++)
+                {
+                    var si = x + s;
+                    if (si >= gridWidth || grid[si, y] != startColor) break;
+                    count++;
+                    
+                    if (count >= 4) return true;
+                }
+            }
+        
+        // Vertical scan for 4 chips across the grid
+        for (var x = 0; x < gridWidth; x++)
+        for (var y = 0; y < gridHeight; y++)
+        {
+            var startColor = grid[x, y];
+            if (startColor == CellState.Empty) continue; // Skip empty cells
+
+            var count = 0;
+            for (var s = 0; s < 4; s++)
+            {
+                var si = y + s;
+                if (si >= gridWidth || grid[x, si] != startColor) break;
+                count++;
+                    
+                if (count >= 4) return true;
+            }
+        }
+        
+        // bottom left to top right diagonal scan for 4 chips across the grid
+        for (var x = 0; x < gridWidth; x++)
+        for (var y = 0; y < gridHeight; y++)
+        {
+            var startColor = grid[x, y];
+            if (startColor == CellState.Empty) continue; // Skip empty cells
+
+            var count = 0;
+            for (var s = 0; s < 4; s++)
+            {
+                var siX = x + s;
+                var siY = y + s;
+                if (siX >= gridWidth || siY >= gridHeight || grid[siX, siY] != startColor) break;
+                count++;
+                    
+                if (count >= 4) return true;
+            }
+        }
+        
+        // top left to bottom right diagonal scan for 4 chips across the grid
+        for (var x = 0; x < gridWidth; x++)
+        for (var y = 0; y < gridHeight; y++)
+        {
+            var startColor = grid[x, y];
+            if (startColor == CellState.Empty) continue; // Skip empty cells
+
+            var count = 0;
+            for (var s = 0; s < 4; s++)
+            {
+                var siX = x + s;
+                var siY = y - s;
+                if (siX >= gridWidth || siY < 0 ||  siY >= gridHeight || grid[siX, siY] != startColor) break;
+                count++;
+                    
+                if (count >= 4) return true;
+            }
+        }
+        
+        return false;
     }
 }
